3e7aba7a50a0f9c5f9fe0cf43d92b29754766afc
Peniel Tambama <tambama.peniel@outlook.com>
2025-05-07 13:37:03 +0200

    Main from TP

--- RNPM.Common/Migrations/RnpmDbContextModelSnapshot.cs
+++ RNPM.Common/Migrations/RnpmDbContextModelSnapshot.cs
@@ -17,7 +17,7 @@ namespace RNPM.Common.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "8.0.7")
+                .HasAnnotation("ProductVersion", "9.0.4")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128);
 
             SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder);
